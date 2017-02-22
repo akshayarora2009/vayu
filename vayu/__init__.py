01bb2f51da52a27ed7b65090ae2e851d5a0e45fa
Akshay Arora <akshayarora2009@gmail.com>
2017-02-22 22:36:59 +0530

    Adding a basic dynamic routing mechanism

--- vayu/__init__.py
+++ vayu/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
-from vayu.fabric_scripts import sample
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-
+def home():
     return render_template("index.html")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+
+@app.route('/deployments')
+def deployments():
+    return render_template("deployments.html")
+
+
+@app.route('/monitoring')
+def monitoring():
+    return render_template("monitoring.html")
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
